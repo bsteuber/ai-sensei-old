68fb46751923f7052abb5ac8b6856a2117331d23
Benjamin Teuber <bsteuber@gmail.com>
2021-12-06 14:02:23 +0100

    Update

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,4 +1,6 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
-  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer"
+  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
+  WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
+  ENABLE_LIVE_MODE: "true"
 };
